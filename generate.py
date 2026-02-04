77a2de743bdf177c2ecaa9b81df84475f952eca0
Max H. Koren <mhkoren@afs-enclave.com>
2026-02-04 13:27:06 -0500

    karnlands

---
+++ generate.py
@@ -0,0 +1,43 @@
+import re
+from collections import defaultdict
+from dataclasses import dataclass
+from jinja2 import Template
+
+@dataclass
+class Review:
+	card: str
+	rating: int
+	review: list[str]
+
+
+reviews_by_year = defaultdict(list)
+
+with open('karnlands.txt', encoding='utf-8') as f:
+	current_year = None
+	current_card = None
+	for line in f:
+		line = line.strip()
+		if not line:
+			continue
+			
+		elif re.match(r'\d{4}', line):
+			current_year = line.strip()
+		
+		elif re.search(r': See', line):
+			card, see = line.split(': See')
+			reviews_by_year[current_year].append(Review(card, 0, [f"See: {see}"]))
+
+		elif re.search(r'\s[*?]', line):
+			*name_tokens, rating = line.split()
+			current_card = Review(' '.join(name_tokens), len(rating) if rating != '?' else 0, [])
+			reviews_by_year[current_year].append(current_card)
+		
+		else:
+			current_card.review.append(line)
+
+
+with open('page.html.jinja') as f:
+    template = Template(f.read())
+
+with open('index.html', 'w', encoding='utf-8') as f:
+	f.write(template.render(reviews_by_year=reviews_by_year, star='★'))
